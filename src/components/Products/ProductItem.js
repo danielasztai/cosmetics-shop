f89c3fc59915595b60a7ccce050a5ce7c9c96444
danielasztai <daniel.asztai718@gmail.com>
2023-08-06 19:17:51 +0200

    final commit

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 
 import Button from '../UI/Button';
@@ -8,12 +8,13 @@ import { CartContext } from '../../store/cart-context';
 
 const ProductItem = props => {
   const cart = useContext(CartContext);
+  const { pathname } = useLocation();
 
   const products = (Object.entries = props.products);
 
   return products.map(item => (
     <li key={item.id} className={classes.box}>
-      <Link to={`${item.id}`}>
+      <Link to={pathname === '/products' ? `${item.id}` : ''}>
         <img src={item.img} alt={item.title} />
         <h3>{item.title}</h3>
         <p>{item.price}$</p>
